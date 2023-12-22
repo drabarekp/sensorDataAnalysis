a11e068576ca679f2430a9b954071c266429b8c0
szgorski <101061451+szgorski@users.noreply.github.com>
2023-12-22 09:25:42 +0100

    notebook added

--- ActivityDictionary.py
+++ ActivityDictionary.py
@@ -11,3 +11,9 @@ WALKSLOW = "walkslow"
 activities_names = [DOWNSTAIRS, JOGGING, LYING, SITTING, STANDING, UPSTAIRS, WALKFAST, WALKMOD, WALKSLOW]
 intense_activities_names = [DOWNSTAIRS, JOGGING, UPSTAIRS, WALKFAST, WALKMOD, WALKSLOW]
 passive_activities_names = [LYING, SITTING, STANDING]
+
+person_all = [26, 27, 28, 29, 30, 31, 33, 34, 36, 39, 40, 41, 42, 43, 44, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56,
+              57, 58, 59, 60, 61, 62, 63]
+person_train = [26, 27, 28, 29, 30, 31, 33, 34, 36, 39, 40, 41, 42, 43, 44, 46, 47, 48, 49, 50, 51, 52]
+person_validate = [53, 54, 55, 56, 57, 58]
+person_test = [59, 60, 61, 62, 63]
